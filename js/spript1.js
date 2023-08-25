06cb376d8c402665ddccae43e39aee366b2a8525
matheus <matheusfavoreto@gamil.com>
2023-08-25 10:14:00 -0300

    adiciona arquivos do projeto ao repositorio

---
+++ js/spript1.js
@@ -0,0 +1,23 @@
+class produto {
+
+  constructor() {
+        this.id = 0;
+        this.nomeproduto = '';
+        this.valor = 0;
+  }
+    
+    adicionar() {
+         alert('vamos adicionar um produto!');
+        //...
+    }
+
+   excluir() {
+
+       //..
+
+       alert('item deletado')
+   }
+
+}
+
+var porduto = new produto();
